1b21e08a059a03667db26f53cae7edfe1eb0485b
Ferin.Chow <nrrri.cho@gmail.com>
2025-04-21 20:17:32 -0400

    fix: bug

--- src/components/WebDevProject.tsx
+++ src/components/WebDevProject.tsx
@@ -5,10 +5,6 @@ function WebDevProject() {
     const jobSkill = ['web Developer', 'mobile application Developer', 'ux/ui designer']
     const skillTitle = 'web development projects'
 
-    // const checkScreenCondition = (idx: number) => {
-    //     return idx !== jobSkill.length - 1 && window.innerWidth <= 768
-    // }
-
     const skillHeader = () => {
         return (
             <div className="flex flex-col items-center justify-center">
@@ -16,13 +12,13 @@ function WebDevProject() {
                     <p className="text-secondary200 absolute">{skillTitle}</p>
                     <p className="m-2px text-primary100">{skillTitle}</p>
                 </div>
-                {<hr className={`sm:hidden border border-b-[1px] border-[#EFEBE5] w-2/3 mb-2`} />}
+                {<hr className={`md:hidden border border-b-[1px] border-[#EFEBE5] w-2/3 mb-2`} />}
                 <div className="mb-4 flex md:flex-row items-center flex-col uppercase text-xl text-grey300 text-light gap-3">
                     {jobSkill.map((job, idx) => {
                         return (
-                            <div key={idx}>
+                            <div key={idx} className="flex flex-row justify-around gap-3">
                                 <p >{job}</p>
-                                {/* {checkScreenCondition(idx) && <p>|</p>} */}
+                                {(idx !== jobSkill.length - 1) && <p className="md:flex hidden" >|</p>}
                             </div>
                         )
                     })}
